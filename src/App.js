9140b10cec9f4511517565942b9cfa018fd6e4dd
Devan Fischer <contact@devanfischer.com>
2022-04-27 08:50:04 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, { useEffect, useContext, useState } from 'react';
+import { BrowserRouter as Router, Link, Route, Routes, useParams } from 'react-router-dom';
+import axios from 'axios';
+
 import './App.css';
+import { clientTokens } from './static/client/client_tokens';
+import { getManyChatToken } from './utils/getManyChatToken';
+// MUI
+import { Box } from '@mui/material';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [userData, setUserData] = useState({});
+
+  const { company, userId } = useParams();
+  const mcToken = getManyChatToken(company, clientTokens);
+
+  useEffect(() => {
+    console.log(company, userId);
+    console.log(mcToken);
+
+    // Get user data from ManyChat. Takes in userId and mcToken
+    const mcUrl = `https://api.manychat.com/fb/subscriber/getInfo?subscriber_id=${userId}`;
+    const corsBypassUrl = `https://cors-anywhere.herokuapp.com/${mcUrl}`;
+    const config = {
+      method: 'GET',
+      mode: 'cors',
+      headers: { 'Content-Type': 'application/json', Authorization: mcToken },
+    };
+    fetch(corsBypassUrl, config)
+      .then((response) => response.json())
+      .then((data) => {
+        setUserData(data.data);
+        console.log(userData);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Box sx={{ background: 'red' }}>Hello from V2</Box>
       </header>
     </div>
   );
 }
 
 export default App;
+
+//  Get disclosures from CGM
+// def get_cgm_disclosures(user):
+//     payload = {"Token": user["token"], "PackageID": user["package_id"], "ResidenceState": user["state"], "TribalResident": user["tribal"], "EligibilityProgram": user["benefit"]}  # Form request data
+
+//     if user["test_or_live"] == "t":
+//         url = "https://lifeline-preprod.cgmllc.net/api/v2/disclosuresconfiguration"
+//     else:
+//         url = "https://lifeline.cgmllc.net/api/v2/disclosuresconfiguration"
+
+//     response = requests.post(url, headers={"Content-Type": "application/x-www-form-urlencoded"}, data=payload)  # Make request to CGM
+//     response_json = json.loads(response.text)
+
+//     if response_json["Status"] == "Failure":
+//         print("CGM RESPONSE", json.dumps(response_json, indent=2))
+
+//     user["ieh"] = str(response_json["CaptureIehForm"]).lower()  # Update User fields
+//     user["items"] = response_json["DisclosureItems"]
+//     return user
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Landing from './components/landing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/:company/:userId" element={<App />} />
+    </Routes>
+  </Router>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
